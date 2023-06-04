bd983f51450178a79acaea8108ead22cfb429ae7
Nico <valdivialopez.nicolas@gmail.com>
2023-06-03 23:02:16 -0400

    agregando funcionalidad a los checkout

--- src/app/bd/Conexion.java
+++ src/app/bd/Conexion.java
@@ -12,10 +12,10 @@ public class Conexion {
 
 
     //String url = "D:\\documentos\\universidad\\paradigmas\\tareas\\tarea2\\Zoo\\animales.db";
-    String url = "C:\\Users\\maxor\\OneDrive\\Escritorio\\Zoo\\Zoo\\animales.db";
+    //String url = "C:\\Users\\maxor\\OneDrive\\Escritorio\\Zoo\\Zoo\\animales.db";
    //String url = "C:\\Users\\rafis\\OneDrive\\Escritorio\\tarea 2\\Zoo\\animales.db";
    //String url = "C:\\Users\\nicox\\OneDrive\\Escritorio\\Zoo\\animales.db";
-    //String url = "C:\\Users\\Nico\\Desktop\\github\\Zoo\\animales.db";
+    String url = "C:\\Users\\Nico\\Desktop\\github\\Zoo\\animales.db";
 
    
 
